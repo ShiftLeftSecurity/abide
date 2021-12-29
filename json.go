acf5ff9bffffdbfa14941a6d088210375e00a519
Alex Hornbake <alex@shiftleft.io>
2021-12-29 18:02:52 +0100

    fix more staticchecks

--- json.go
+++ json.go
@@ -9,23 +9,22 @@ func updateKeyValuesInMap(key string, value interface{}, m map[string]interface{
 // Recursively update the map to update the specified key.
 func updateMap(key string, value interface{}, m map[string]interface{}) map[string]interface{} {
 	for k, v := range m {
-		switch v.(type) {
+		switch s := v.(type) {
 		// If slice, iterate through each entry and call updateMap
 		// only if it's a map[string]interface{}.
 		case []interface{}:
-			for i := range v.([]interface{}) {
-				switch v.([]interface{})[i].(type) {
+			for i := range s {
+				switch s[i].(type) {
 				case map[string]interface{}:
 					v.([]interface{})[i] = updateMap(key, value, v.([]interface{})[i].(map[string]interface{}))
 				}
 			}
 		case map[string]interface{}:
-			m[k] = updateMap(key, value, v.(map[string]interface{}))
+			m[k] = updateMap(key, value, s)
 		default:
 			if k == key {
 				m[k] = value
 			}
-			break
 		}
 	}
 
